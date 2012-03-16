4604ab5acfc8f7945cbd1d99ba6d74161224dca9
panic <panic@ffed5ea4-548f-4068-9d18-8ed96cdef38e>
2012-03-16 11:16:39 +0000

    First working version of mapconvert.py (reads hex terrain,road and names)
    git-svn-id: svn://hall.linuxconsulting.ro/websites/linuxconsulting-ro/code-repository/repository/html5-hexgrid@114 ffed5ea4-548f-4068-9d18-8ed96cdef38e

--- tools/map/mapconvert.py
+++ tools/map/mapconvert.py
@@ -1,40 +1,114 @@
 import os, sys
 from pprint import pprint
 from struct import *
+from lxml import etree as x
 
+# spaghetti !
 #file specs http://luis-guzman.com/links/PG2_FilesSpec.html#(MAP) file
+MAP_IMAGE_URL="resources/maps/images/"
 
-# list of tuples (terrain, road)
-h = []
+# returns a string with all the hexes on the map file
+def get_map_hexes(f):
+    pos = f.tell()
+    f.seek(10) # where the hex info starts
+    data = f.read(12600) # 45x40x7 bytes
+    f.seek(pos)
+    return data
+
+# returns a string with all the hexes on the scenario file
+def get_scn_hexes(f):
+    pos = f.tell()
+    f.seek(22 + 388) # there the hex info starts in scn file
+    data = f.read(10800) # 45x40x6 bytes
+    f.seek(pos)
+    return data
+
+# returns map name string UPPERCASE
+def get_scn_map_name(f):
+    pos = f.tell()
+    f.seek(22 + 388 + 10800)
+    data = f.read(20)
+    f.seek(pos)
+    return data.upper().strip('\0')
+
+
+# returns scenario description
+def get_scn_description(scnfile, text):
+    pos = scnfile.tell()
+    scnfile.seek(1+2+1)
+    txtpos = unpack('H', scnfile.read(2))[0]
+    desc = text[txtpos].rstrip()
+    scnfile.seek(pos)
+    return desc
+
+# return map image name, cols and rows
+def get_map_info(f):
+    mapinfo = []
+    pos = f.tell()
+    f.seek(0)
+    mapimgname = "map_" + str(unpack('h',f.read(2))[0]) + ".png"
+    mapinfo.append(mapimgname)
+    mapinfo.append(unpack('h',f.read(2))[0])
+    mapinfo.append(unpack('h',f.read(2))[0])
+    f.seek(pos)
+    return mapinfo
 
 #for map in sys.argv[1:]:
-for map in ["DESSAU.MAP"]:
-    print "Processing %s\n" % map
-    f = open(map, 'rb')
-    mapnr = unpack('h',f.read(2))[0]
-    cols = unpack('h',f.read(2))[0]
-    rows = unpack('h',f.read(2))[0]
-    print "Map: %d Cols: %d Rows: %d \n" % (mapnr,cols,rows)
-    f.seek(f.tell() + 2 + 2) # skip unknown fields
+for scn in ["CAENUK.SCN"]:
+    print "Processing %s\n" % scn
+    # the corresponding scenarion txt name
+    tf = open(os.path.splitext(scn)[0] + ".TXT",'r')
+    sf = open(scn, 'rb')
+    mf = open(get_scn_map_name(sf), 'rb')
+    # contains all names list from the txt file
+    scntext = tf.readlines()
+    
+    mapimgname, cols, rows = get_map_info(mf)
+    scndesc = get_scn_description(sf, scntext)
+    
+    #xmlmap = x.Element('map', name="" , description="", rows="", cols="" ,image="")
+    xmlmap = x.Element('map')
+    xmlmap.set("name", scn)
+    xmlmap.set("description", scndesc)
+    xmlmap.set("rows", str(rows))
+    xmlmap.set("cols", str(cols))
+    xmlmap.set("image", MAP_IMAGE_URL + mapimgname)
+    
     col = row = 0
     # maps always define 45x40 hexes x 7 bytes first 2 being terrain and road
-    data = f.read(12600)
-    offset = 0
+    # need to skip those rows,cols that aren't needed
+    maphdata = get_map_hexes(mf)
+    scnhdata = get_scn_hexes(sf)
+    mapoffset = 0
+    scnoffset = 0
     while True:
-        s = unpack('HHHc', data[offset:offset+7])
-        print s[0:2],    
+        hm = unpack('HHHc', maphdata[mapoffset:mapoffset + 7])
+        hs = unpack('bbbbH', scnhdata[scnoffset:scnoffset + 6])
+	terrain,road =  hm[0:2]
+	name = ""
+	textpos = hs[4] - 1  #file index to array index
+	if textpos > 0:
+	    name = scntext[textpos].rstrip()
+	
+	tmpnode = x.SubElement(xmlmap, "hex")
+	tmpnode.set("row", str(row))
+	tmpnode.set("col", str(col))
+	tmpnode.set("terrain", str(terrain))
+	tmpnode.set("road", str(road))
+	tmpnode.set("name", str(name))
+	
         col = col + 1
-        offset = offset + 7
+        mapoffset = mapoffset + 7
+        scnoffset = scnoffset + 6
         if col == cols:
             col = 0
             row = row + 1
-            offset = offset + (45 - cols)*7
-            print offset
-            print
+            mapoffset = mapoffset + (45 - cols) * 7
+            scnoffset = scnoffset + (45 - cols) * 6
+            #print mapoffset, scnoffset
+            #print
         if row == rows:
             break 
-           
-    #h.append(data[0:2]) #only terrain type and road
-    
-    #for i in range (rows-1):
-    #print h[i*cols: i*cols+cols]
+            
+    xml = x.ElementTree(xmlmap)
+    xml.write(os.path.splitext(scn)[0].lower() + ".xml", pretty_print=True, xml_declaration=True)
