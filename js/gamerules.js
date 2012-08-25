d718e3026d9b6462e2e92fe61d90c1175b862370
Nicu Pavel <npavel@mini-box.com>
2012-08-25 08:50:16 +0300

    Surprised units don't dismount

--- js/gamerules.js
+++ js/gamerules.js
@@ -371,7 +371,7 @@ GameRules.calculateAttackResults = function(atkunit, defunit)
 	if (isAir(defunit))	dTerrain == terrainType.Clear;
 	
 	//Infantry always dismounts when attacked
-	if (defunit.isMounted && equipment[defunit.eqid].uclass == unitClass.infantry)
+	if (defunit.isMounted && !defunit.isSurprised && equipment[defunit.eqid].uclass == unitClass.infantry)
 			dData = equipment[defunit.eqid];
 	
 	//Attacking unit type
